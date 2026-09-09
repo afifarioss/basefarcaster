d06cd5d82f462a8df6ba60d498003a8497fe26ba
afifarioss.base.eth <afif.peugeot@gmail.com>
2026-09-09 21:05:34 +0800

    marketing: clarify personal tip link

--- components/ProfileCard.tsx
+++ components/ProfileCard.tsx
@@ -62,7 +62,7 @@ export function ProfileCard() {
           onClick={handleCopyLink}
           className="chip w-full !py-2.5 text-xs"
         >
-          {copied ? "Copied! Paste it in a cast ⚡" : "Get your tip link"}
+          {copied ? "Copied! Share it to get tipped ⚡" : "Get your personal tip link"}
         </button>
       )}
     </div>
